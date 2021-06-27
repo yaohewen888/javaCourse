6cfec9ec4dbaa9fd00a50423ccfa983b2ca659e8
yaohewen <542311877@qq.com>
2021-06-27 23:33:17 +0800

    homework done

--- src/homework/week1/HelloXlassClassLoader.java
+++ src/homework/week1/HelloXlassClassLoader.java
@@ -1,21 +1,71 @@
 package homework.week1;
 
-import java.util.Base64;
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class HelloXlassClassLoader extends ClassLoader {
 	
-	@Override
-    protected Class<?> findClass(String name) throws ClassNotFoundException {
-		String helloBase64 = "yv66vgAAADQAHAoABgAOCQAPABAIABEKABIAEwcAFAcAFQEABjxpbml0PgEAAygpVgEABENvZGUBAA9MaW5lTnVtYmVyVGFibGUBAAg8Y2xpbml0PgEAClNvdXJjZUZpbGUBAApIZWxsby5qYXZhDAAHAAgHABYMABcAGAEAGEhlbGxvIENsYXNzIEluaXRpYWxpemVkIQcAGQwAGgAbAQAJanZtL0hlbGxvAQAQamF2YS9sYW5nL09iamVjdAEAEGphdmEvbGFuZy9TeXN0ZW0BAANvdXQBABVMamF2YS9pby9QcmludFN0cmVhbTsBABNqYXZhL2lvL1ByaW50U3RyZWFtAQAHcHJpbnRsbgEAFShMamF2YS9sYW5nL1N0cmluZzspVgAhAAUABgAAAAAAAgABAAcACAABAAkAAAAdAAEAAQAAAAUqtwABsQAAAAEACgAAAAYAAQAAAAMACAALAAgAAQAJAAAAJQACAAAAAAAJsgACEgO2AASxAAAAAQAKAAAACgACAAAABQAIAAYAAQAMAAAAAgAN";
-        byte[] bytes = decode(helloBase64);
-		return defineClass(name, bytes, 0, bytes.length);
+	private byte[] InputStream2ByteArray(String filePath) throws IOException {
+		 
+	    InputStream in = new FileInputStream(filePath);
+	    byte[] data = toByteArray(in);
+	    in.close();
+	 
+	    return data;
+	}
+	 
+	private byte[] toByteArray(InputStream in) throws IOException {
+	 
+	    ByteArrayOutputStream out = new ByteArrayOutputStream();
+	    byte[] buffer = new byte[1024 * 4];
+	    int n = 0;
+	    while ((n = in.read(buffer)) != -1) {
+	        out.write(buffer, 0, n);
+	    }
+	    return out.toByteArray();
 	}
 	
-	public byte[] decode(String base64) {
-        return Base64.getDecoder().decode(base64);
+	private static byte[] decode(byte[] byteArray) {
+        byte[] targetArray = new byte[byteArray.length];
+        for (int i = 0; i < byteArray.length; i++) {
+            targetArray[i] = (byte) (255 - byteArray[i]);
+        }
+        return targetArray;
     }
 	
-	public static void main(String[] args) {
-
+	@Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+		byte[] bytes = null;
+		try {
+			bytes = InputStream2ByteArray("D:/project/workspace/git/javaCourse/Hello.xlass");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		byte[] classBytes = decode(bytes);
+		return defineClass(name, classBytes, 0, classBytes.length);
+	}
+	
+	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
+		// 相关参数
+        final String className = "Hello";
+        final String methodName = "hello";
+        // 创建类加载器
+        ClassLoader classLoader = new HelloXlassClassLoader();
+        // 加载相应的类
+        Class<?> clazz = classLoader.loadClass(className);
+        // 看看里面有些什么方法
+        for (Method m : clazz.getDeclaredMethods()) {
+            System.out.println(clazz.getSimpleName() + "." + m.getName());
+        }
+        // 创建对象
+        Object instance = clazz.getDeclaredConstructor().newInstance();
+        // 调用实例方法
+        Method method = clazz.getMethod(methodName);
+        method.invoke(instance);
 	}
 }
