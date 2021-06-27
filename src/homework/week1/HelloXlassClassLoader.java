a137d6b9b65738b7390db3c9b0e90e98df2a07a0
yaohewen <542311877@qq.com>
2021-06-27 21:49:54 +0800

    debug video test

--- src/homework/week1/HelloXlassClassLoader.java
+++ src/homework/week1/HelloXlassClassLoader.java
@@ -1,8 +1,21 @@
 package homework.week1;
 
+import java.util.Base64;
+
 public class HelloXlassClassLoader extends ClassLoader {
 	
+	@Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+		String helloBase64 = "yv66vgAAADQAHAoABgAOCQAPABAIABEKABIAEwcAFAcAFQEABjxpbml0PgEAAygpVgEABENvZGUBAA9MaW5lTnVtYmVyVGFibGUBAAg8Y2xpbml0PgEAClNvdXJjZUZpbGUBAApIZWxsby5qYXZhDAAHAAgHABYMABcAGAEAGEhlbGxvIENsYXNzIEluaXRpYWxpemVkIQcAGQwAGgAbAQAJanZtL0hlbGxvAQAQamF2YS9sYW5nL09iamVjdAEAEGphdmEvbGFuZy9TeXN0ZW0BAANvdXQBABVMamF2YS9pby9QcmludFN0cmVhbTsBABNqYXZhL2lvL1ByaW50U3RyZWFtAQAHcHJpbnRsbgEAFShMamF2YS9sYW5nL1N0cmluZzspVgAhAAUABgAAAAAAAgABAAcACAABAAkAAAAdAAEAAQAAAAUqtwABsQAAAAEACgAAAAYAAQAAAAMACAALAAgAAQAJAAAAJQACAAAAAAAJsgACEgO2AASxAAAAAQAKAAAACgACAAAABQAIAAYAAQAMAAAAAgAN";
+        byte[] bytes = decode(helloBase64);
+		return defineClass(name, bytes, 0, bytes.length);
+	}
+	
+	public byte[] decode(String base64) {
+        return Base64.getDecoder().decode(base64);
+    }
+	
 	public static void main(String[] args) {
-		System.out.println("test");
+
 	}
 }
